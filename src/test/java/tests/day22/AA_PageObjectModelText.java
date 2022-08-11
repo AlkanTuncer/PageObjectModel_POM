4fe212ef9974442912eac42f9ab48255fc0f6bea
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-11 19:18:14 +0300

    PageObjectModel_POM _ Framework Setup and AmazonSearchTest

---
+++ src/test/java/tests/day22/AA_PageObjectModelText.java
@@ -0,0 +1,25 @@
+package tests.day22;
+
+public class AA_PageObjectModelText {
+
+        /*
+                      PAGE OBJECT MODEL
+
+            - POM popüler bir Framework Design Pattern'dir.
+
+            - Test suitlerimizde cok fazla testimiz oldugunda, test caseleri ve kodları korumak
+            daha karmasık hale gelir.Bu nedenle;
+                - sürdürülebilir / maintainable
+                - yeniden kullanılabilir / reusable
+                - daha hızlı / faster
+                - anlaşılabilir / understandable
+            daha iyi bir framework dizaynına ihtiyacımız var.
+
+            - Page Object Model ile sayfaya özgü elementleri veya metotları page class içinde tutar ve
+            bunları gercek test classlarından uzak tutarız.
+
+            - POM ile ihtiyacımız olan class üyelerini sadece bir kez create edip birden cok kez kullanabiliriz.
+
+     */
+
+}
--- src/test/java/tests/day22/AmazonAramaTest.java
+++ src/test/java/tests/day22/AmazonAramaTest.java
@@ -17,23 +17,4 @@ public class AmazonAramaTest {
         Driver.closeDriver();
     }
 
-    /*
-                      PAGE OBJECT MODEL
-
-            - POM popüler bir Framework Design Pattern'dir.
-
-            - Test suitlerimizde cok fazla testimiz oldugunda, test caseleri ve kodları korumak
-            daha karmasık hale gelir.Bu nedenle;
-                - sürdürülebilir / maintainable
-                - yeniden kullanılabilir / reusable
-                - daha hızlı / faster
-                - anlaşılabilir / understandable
-            daha iyi bir framework dizaynına ihtiyacımız var.
-
-            - Page Object Model ile sayfaya özgü elementleri veya metotları page class içinde tutar ve
-            bunları gercek test classlarından uzak tutarız.
-
-            - POM ile ihtiyacımız olan class üyelerini sadece bir kez create edip birden cok kez kullanabiliriz.
-
-     */
 }
