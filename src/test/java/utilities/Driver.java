b8b02270a89f15e0adb64a0f1745d888c139ef22
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-11 19:12:05 +0300

    PageObjectModel_POM _ Framework Setup

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,7 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
@@ -25,6 +27,14 @@ public class Driver {
                 case "safari" :
                     WebDriverManager.safaridriver().setup();
                     driver = new SafariDriver();
+                    break;
+                case "ie" :
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
                     break;
             }
         }
