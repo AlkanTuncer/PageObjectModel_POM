63a7f95731c1cb85b7a9f420e48e9be4041790c5
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-11 20:11:56 +0300

    PageObjectModel_POM _ AmazonSearchTest

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class AmazonPage {
 
     public AmazonPage() {
@@ -15,6 +17,12 @@ public class AmazonPage {
     public WebElement aramaKutusu;
 
     @FindBy(xpath = "(//*[text()='Manchester City FC Puma Boys’ and Girls 2022/23 Replica Home Football Jersey'])[1]")
-    public WebElement ilkUrun;
+    public WebElement firstProduct;
+
+    @FindBy(xpath = "//span[@id=\"productTitle\"]")
+    public WebElement firstProductTitle;
+
+    @FindBy(xpath = "//span[@class=\"a-size-base-plus a-color-base a-text-normal\"]")
+    public List<WebElement> productTitles;
 
 }
--- src/test/java/tests/day22/AmazonAramaTest.java
+++ src/test/java/tests/day22/AmazonAramaTest.java
@@ -1,6 +1,7 @@
 package tests.day22;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AmazonPage;
@@ -15,9 +16,16 @@ public class AmazonAramaTest {
     public void test(){
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
         amazonPage.aramaKutusu.sendKeys("Manchester City", Keys.ENTER);
-        amazonPage.ilkUrun.click();
+
+        for (WebElement allTitles: amazonPage.productTitles){
+            System.out.println(allTitles.getText());
+        }
+
+
+        amazonPage.firstProduct.click();
 
         Assert.assertTrue(Driver.getDriver().getTitle().contains("Manchester City"),"Başlık 'Manchester City' içermiyor.");
+        Assert.assertTrue(amazonPage.firstProductTitle.getText().contains("Manchester City"),"Ürün başlığı 'Manchester City' içermiyor.");
 
         Driver.closeDriver();
     }
