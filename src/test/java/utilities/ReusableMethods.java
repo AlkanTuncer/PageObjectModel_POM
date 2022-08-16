d7432633fb6333a7d2d6207db0fea72eac3f1453
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-16 20:13:14 +0300

    Page Object Model _ POM _ utilities - ReusableMethods & Excel & TestBaseRapor

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -19,14 +19,14 @@ import java.util.List;
 
 public class ReusableMethods {
 
-    public static String takeScreenshot(String name){
+    public static String takeScreenshot(String name) {
         String date = new SimpleDateFormat("yyyyMMddhhss").format(new Date());
-        TakesScreenshot ts= (TakesScreenshot) Driver.getDriver();
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
         File source = ts.getScreenshotAs(OutputType.FILE);
-        String target = System.getProperty("user.dir")+"src/test-output/screenshots"+name+date+".png";
+        String target = System.getProperty("user.dir") + "/test-output/screenshots" + name + date + ".png";
         File file = new File(target);
         try {
-            FileUtils.copyFile(source,file);
+            FileUtils.copyFile(source, file);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,30 +34,70 @@ public class ReusableMethods {
         return target;
     }
 
-    public static void hover(WebElement element){
+    //========Switching Window=====//
+    public static void switchToWindow(String targetTitle) {
+        String origin = Driver.getDriver().getWindowHandle();
+        for (String handle : Driver.getDriver().getWindowHandles()) {
+            Driver.getDriver().switchTo().window(handle);
+            if (Driver.getDriver().getTitle().equals(targetTitle)) {
+                return;
+            }
+        }
+        Driver.getDriver().switchTo().window(origin);
+    }
+
+    //========Hover Over=====//
+    public static void hover(WebElement element) {
         Actions actions = new Actions(Driver.getDriver());
         actions.moveToElement(element).perform();
     }
 
-    public static List<String> getElementsText(By locator){
+    //==========Return a list of string given a list of Web Element====//
+    public static List<String> getElementsText(List<WebElement> list) {
+        List<String> elemTexts = new ArrayList<>();
+        for (WebElement el : list) {
+            if (!el.getText().isEmpty()) {
+                elemTexts.add(el.getText());
+            }
+        }
+        return elemTexts;
+    }
+
+    //========Returns the Text of the element given an element locator==//
+    public static List<String> getElementsText(By locator) {
         List<WebElement> elements = Driver.getDriver().findElements(locator);
         List<String> elementsTexts = new ArrayList<>();
-        for (WebElement elem : elements){
-            if (!elem.getText().isEmpty()){
+        for (WebElement elem : elements) {
+            if (!elem.getText().isEmpty()) {
                 elementsTexts.add(elem.getText());
             }
         }
         return elementsTexts;
     }
 
+    //   HARD WAIT WITH THREAD.SLEEP
+    //   waitFor(5);  => waits for 5 second
+    public static void waitFor(int sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     // ------------------ Explicit Wait ------------------------
-    public static WebElement waitForVisibility(WebElement element, int time){
+    public static WebElement waitForVisibility(WebElement element, int time) {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(time));
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    public static WebElement waitForVisibility(By locator, int time){
+    public static WebElement waitForVisibility(By locator, int time) {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(time));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static WebElement waitForClickability(WebElement element, int timeout) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 }
