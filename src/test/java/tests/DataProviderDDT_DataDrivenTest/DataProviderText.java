7079db4d4b4c20cb0350570799896d3569ad2a7f
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-24 18:56:41 +0300

    TestNG - DataProvider & DDT(DataDrivenTest)

---
+++ src/test/java/tests/DataProviderDDT_DataDrivenTest/DataProviderText.java
@@ -0,0 +1,13 @@
+package tests.DataProviderDDT_DataDrivenTest;
+
+public class DataProviderText {
+
+    /*
+          - @DataProvider bir TestNG annotation'idir.
+          - Sadece TestNG ile kullanılır.
+          - Veri saglamak için kullanılır.
+          - DDT (Data Driven Test) yapılır.
+          - Cucumber'daki Scenario Outline ile aynı işleve sahiptir.
+     */
+
+}
