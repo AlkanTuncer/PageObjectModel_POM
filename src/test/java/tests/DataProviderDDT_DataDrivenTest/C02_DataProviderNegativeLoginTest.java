7e4a7e4b70172f95d67131a8d4220be04988b665
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-24 19:27:01 +0300

    TestNG - DataProvider & DDT(DataDrivenTest) - NegativeLoginTest

--- src/test/java/tests/DataProviderDDT_DataDrivenTest/C02_DataProviderNegativeLoginTest.java
+++ src/test/java/tests/DataProviderDDT_DataDrivenTest/C02_DataProviderNegativeLoginTest.java
@@ -1,7 +1,37 @@
 package tests.DataProviderDDT_DataDrivenTest;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.HotelMyCampPage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
 public class C02_DataProviderNegativeLoginTest {
 
+    @DataProvider
+    public static Object[][] hmcTestData() {
+        String[][] testData = {  {ConfigurationReader.getProperty("HMCWrongUsername"),ConfigurationReader.getProperty("HMCValidPassword")} ,
+                                 {ConfigurationReader.getProperty("HMCValidUsername"),ConfigurationReader.getProperty("HMCWrongPassword")} ,
+                                 {ConfigurationReader.getProperty("HMCWrongUsername"),ConfigurationReader.getProperty("HMCWrongPassword")}  };
+        return testData;
+    }
 
+    @Test(dataProvider = "hmcTestData")
+    public void negativeLoginTest(String username,String password){
+        Driver.getDriver().get(ConfigurationReader.getProperty("urlHMC"));
+
+        HotelMyCampPage hotelMyCampPage = new HotelMyCampPage();
+
+        hotelMyCampPage.logInButton.click();
+        hotelMyCampPage.username.sendKeys(username);
+        hotelMyCampPage.password.sendKeys(password);
+        hotelMyCampPage.btnSubmit.click();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(hotelMyCampPage.wrongDataText.isDisplayed());
+        softAssert.assertAll();
+
+    }
 
 }
